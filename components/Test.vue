332c51770714ac2f24916eca5e18ccb84639ce47
damien anselmi <anselmidamien@gmail.com>
2021-09-18 21:58:57 +0200

    add landing page

---
+++ components/Test.vue
@@ -0,0 +1,51 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="grid-container">
+    <span class="name-box">
+      Ea Bla Yoga
+    </span>
+    <div class="menu-box">
+      <div class="menu-items">
+        <v-btn>Menu 1</v-btn>
+        <v-btn>Menu 2</v-btn>
+        <v-btn>Menu 3</v-btn>
+        <v-btn>Menu 4</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap");
+.grid-container {
+  color: white;
+  display: grid;
+  grid-template-columns: 33.3vw 33.3vw 33.3vw;
+  grid-template-rows: 16.6vh 16.6vh 16.6vh 16.6vh 16.6vh 16.6vh;
+}
+.name-box {
+  grid-column: 2 / 3;
+  grid-row: 5 / 7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Architects Daughter", cursive;
+  font-size: 50px;
+}
+.menu-box {
+  grid-column: 2 / 4;
+  grid-row: 1 / 2;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50% 50%;
+  font-family: "Architects Daughter", cursive;
+  font-size: 25px;
+}
+.menu-items {
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <Test />
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
+
+<style>
+html {
+  background: url(static/236261430_401501761396117_6121646498851519330_n.jpeg)
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  overflow: hidden;
+  height: 100vh;
+  width: 100vw;
+}
+@import url("https://fonts.googleapis.com/css2?family=Fleur+De+Leah&display=swap");
+</style>
